b1367733d64befa8273da8878fd1b9757dc7324b
Jack <jpgundrum@wisc.edu>
2024-04-03 18:02:44 -0500

    minor updates

--- token_transfer.ts
+++ token_transfer.ts
@@ -3,11 +3,12 @@
 import { wormhole, TokenId, Wormhole } from "@wormhole-foundation/sdk";
 import evm from "@wormhole-foundation/sdk/evm";
 import solana from "@wormhole-foundation/sdk/solana";
-//import { getSigner, waitLog } from "./helpers/index.js"; // Ensure helpers are implemented
-import dotenv from 'dotenv';
+//import { getSigner, waitLog } from "./helpers/index.js"; // Ensure helpers are implemented for signing the transactions
 
-dotenv.config();
-const ADDRESS = process.env.MAPMETRICS as string; // Address of MapMetrics coin
+// uncomment below if you want to import a token address
+// import dotenv from 'dotenv';
+// dotenv.config();
+// const ADDRESS = process.env.MAPMETRICS as string; // Address of MapMetrics coin
 
 async function main() {
     const wh = await wormhole("Testnet", [evm, solana]); // will need to change to Mainnet when trying to transfer
@@ -17,7 +18,10 @@ async function main() {
     const rcvChain = wh.getChain("Ethereum"); // Moonbeam is EVM-compatible
   
     // Prepare the token details for transfer
-     const token = Wormhole.tokenId("Solana", ADDRESS);
+     const token = Wormhole.tokenId("Solana", "123test"); // change to MapMetrics's contract address
      console.log(token);
+
+     // TODO look at sample repo: https://github.com/wormhole-foundation/wormhole-sdk-ts/blob/main/examples/src/tokenBridge.ts
+     // to see how to transfer tokens
   }
 main();
